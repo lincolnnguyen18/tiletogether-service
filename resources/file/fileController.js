be69353e9375b5aa307c9a261dde03c0626c4011
XIAOTMA <49177628+maxt0214@users.noreply.github.com>
2022-10-18 19:56:28 -0400

    Conformed to coding conventions

--- resources/file/fileController.js
+++ resources/file/fileController.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const { File } = require('./fileSchema.js');
+//const { File } = require('./fileSchema.js');
 
 export const FileRouter = express.Router();
 
-FileRouter.post('/', getCommunityFiles);
+/* FileRouter.post('/', getCommunityFiles);
 FileRouter.get('/search/', search);
 FileRouter.get('/user', getUserFiles);
 
-async function getCommunityFiles(req, res) {
+async function getCommunityFiles (req, res) {
 
 }
 
-async function search(req, res) {
+async function search (req, res) {
 
 }
 
-async function getUserFiles(req, res) {
+async function getUserFiles (req, res) {
 
-}
+} */

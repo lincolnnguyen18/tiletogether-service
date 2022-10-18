3a229dde8aeb03e40ecce12a64d717beb55c242e
Lincoln Nguyen <letsmakeaworld@icloud.com>
2022-10-18 19:59:44 -0400

    Update cdk and cors to allow clients to connect

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ import { UserRouter } from './resources/user/userController.js';
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
 export const app = express();
-if (process.env.NODE_ENV === 'development') {
-  app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
-}
+
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,9 +15,3 @@ app.use('/api/users', UserRouter);
 app.get('/health', (_, res) => {
   res.status(200).send('OK');
 });
-
-// TODO: decide to serve static assets from service directly or use S3/CloudFront
-// app.use(express.static('../client/build'));
-// app.get('*', (_, res) => {
-//   res.sendFile('index.html', { root: '../client/build' });
-// });

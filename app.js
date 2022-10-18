8b7bc1bbf20eee049b2051a20d032ccf84b5d43a
Lincoln Nguyen <letsmakeaworld@icloud.com>
2022-10-18 17:13:36 -0400

    Fix startup bug

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import { UserRouter } from './resources/user/userController.js';
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const { UserRouter } = require('./resources/user/userController');
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
-export const app = express();
+const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 }
@@ -17,8 +17,4 @@ app.get('/health', (_, res) => {
   res.status(200).send('OK');
 });
 
-// TODO: decide to serve static assets from service directly or use S3/CloudFront
-// app.use(express.static('../client/build'));
-// app.get('*', (_, res) => {
-//   res.sendFile('index.html', { root: '../client/build' });
-// });
+module.exports = { app };

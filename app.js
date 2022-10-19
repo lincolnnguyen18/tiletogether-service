2f7083506ce25aaf7cf757804cf99fa0ab35371b
XIAOTMA <49177628+maxt0214@users.noreply.github.com>
2022-10-18 20:02:47 -0400

    Fixed import issues

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import { UserRouter } from './resources/user/userController.js';
-import { FileRouter } from './resources/file/fileController';
+import { FileRouter } from './resources/file/fileController.js';
+
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
 export const app = express();
-
-app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,4 +20,8 @@ app.get('/health', (_, res) => {
   res.status(200).send('OK');
 });
 
-module.exports = app;
+// TODO: decide to serve static assets from service directly or use S3/CloudFront
+// app.use(express.static('../client/build'));
+// app.get('*', (_, res) => {
+//   res.sendFile('index.html', { root: '../client/build' });
+// });
